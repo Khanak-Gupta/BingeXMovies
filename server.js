6ae9c21f8c7b012dc7049d8e4c0c5b624f5736af
Khanak Gupta <124119433+Khanak-Gupta@users.noreply.github.com>
2026-02-15 22:37:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://binge-x-movies-frontend.vercel.app'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
